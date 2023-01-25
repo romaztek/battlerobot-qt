b153ab02ed7a9865d0006ee2724b2ae588c0d37f
Roman K <kartarom21@gmail.com>
2023-01-26 02:26:04 +0300

    Changed project filenames for qmake build

---
+++ BattleRobot_ru_RU.ts
@@ -0,0 +1,200 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>ConnectWindow</name>
+    <message>
+        <location filename="ConnectWindow.qml" line="66"/>
+        <source>Select a device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="74"/>
+        <source>Skip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="204"/>
+        <source>Scanning...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="225"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="252"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="322"/>
+        <source>Connecting to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="387"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ConnectWindow.qml" line="397"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlWindow</name>
+    <message>
+        <location filename="ControlWindow.qml" line="64"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="69"/>
+        <location filename="ControlWindow.qml" line="306"/>
+        <source>Not Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="209"/>
+        <source>STOP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="214"/>
+        <source>MIDDLE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="218"/>
+        <location filename="ControlWindow.qml" line="347"/>
+        <source>LEFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="224"/>
+        <location filename="ControlWindow.qml" line="236"/>
+        <location filename="ControlWindow.qml" line="249"/>
+        <location filename="ControlWindow.qml" line="261"/>
+        <source>LOW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="228"/>
+        <location filename="ControlWindow.qml" line="253"/>
+        <source>NORMAL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="232"/>
+        <location filename="ControlWindow.qml" line="257"/>
+        <source>HIGH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="243"/>
+        <location filename="ControlWindow.qml" line="363"/>
+        <source>RIGHT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="288"/>
+        <location filename="ControlWindow.qml" line="289"/>
+        <location filename="ControlWindow.qml" line="294"/>
+        <source>Reconnect?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="288"/>
+        <location filename="ControlWindow.qml" line="290"/>
+        <source>NULL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="318"/>
+        <source>Touch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="385"/>
+        <location filename="ControlWindow.qml" line="389"/>
+        <source>FORWARD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ControlWindow.qml" line="399"/>
+        <location filename="ControlWindow.qml" line="404"/>
+        <source>BACKWARD</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Logic</name>
+    <message>
+        <location filename="logic.cpp" line="143"/>
+        <source>Unknown Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="146"/>
+        <source>Host Not Found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="149"/>
+        <source>Service Not Found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="152"/>
+        <source>Network Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="155"/>
+        <source>Unsupported Protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="158"/>
+        <source>Operation Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="161"/>
+        <source>Remote Host Closed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="logic.cpp" line="164"/>
+        <source>No Socket Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyIconLabel</name>
+    <message>
+        <location filename="MyIconLabel.qml" line="35"/>
+        <source>NULL</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyIconRadioButtonLabel</name>
+    <message>
+        <location filename="MyIconRadioButtonLabel.qml" line="36"/>
+        <source>NULL</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="92"/>
+        <source>Reconnect?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
